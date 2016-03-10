4b57183599899114fb32087dfd466bdb8069b431
MAREK W. SADOWSKI <mwsadows@us.ibm.com>
2016-03-10 11:48:53 -0800

    Watson Sentiment Analysis added
    40 minutes simplified app on getting Watson SDK based sentiment
    analysis from IBM Cloud:
    http://blumareks.blogspot.com/2016/03/blue-reporter-v21-gets-cognitive-i
    n.html

---
+++ AppWithWatsonSentiment/AppWithWatsonSentiment/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  AppWithWatsonSentiment
+//
+//  Created by Marek Sadowski on 3/10/16.
+//  Copyright © 2016 Marek Sadowski. All rights reserved.
+//
+
+import UIKit
+import WatsonDeveloperCloud
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textStatusLabel: UILabel!
+    
+    //Watson service init
+    var alchemyApiInstance: AlchemyLanguage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func checkButtonPressed(sender: AnyObject) {
+        NSLog(textField.text!)
+        
+        //checking sentiment
+        
+        //step 1: add instance by APIKEY
+        if alchemyApiInstance == nil {
+            alchemyApiInstance = AlchemyLanguage(apiKey: "844de29ef7761fd309c079582e53bc304c10829f")
+        }
+        
+        //step 2: call the API
+        NSLog("calling alchemy api")
+        var textData = textField.text!
+        alchemyApiInstance.getSentiment(requestType: AlchemyLanguageConstants.RequestType.Text,
+            html: nil,
+            url: nil,
+            text: textData,
+            sentimentType: AlchemyLanguageConstants.SentimentType.Normal)
+            {   (error, entities) in
+                //step 3: read the results
+
+                self.textStatusLabel.text = "status " + (entities.docSentiment?.type!)!
+                NSLog("status " + entities.docSentiment.debugDescription)
+        }
+        
+        
+        
+        
+    }
+
+}
+
